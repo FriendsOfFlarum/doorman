695f71c26138d6b51d8c33e917414f4447de8d54
Rafael Horvat <rafael.horvat@glowingblue.com>
2019-07-10 09:34:08 +0200

    Fixed CSS on the Settings Page (rewrite using flex-box)

--- js/src/admin/components/DoormanSettingsListItem.js
+++ js/src/admin/components/DoormanSettingsListItem.js
@@ -12,38 +12,43 @@ export default class DoormanSettingsListItem extends Component {
         var remaining = this.doorkey.maxUses() - this.doorkey.uses();
 
         return (
-            <div style="float: left; margin-bottom: 10px;">
-                <input
-                    className="FormControl Doorkey-key"
-                    type="text"
-                    value={this.doorkey.key()}
-                    placeholder={app.translator.trans('reflar-doorman.admin.page.doorkey.key')}
-                    onchange={m.withAttr('value', this.updateKey.bind(this, this.doorkey))}
-                />
-                {Select.component({
-                    options: this.getGroupsForInput(),
-                    className: 'Doorkey-select',
-                    onchange: this.updateGroupId.bind(this, this.doorkey),
-                    value: this.doorkey.groupId() || 3,
-                })}
-                <input
-                    className="FormControl Doorkey-maxUses"
-                    value={this.doorkey.maxUses() || '0'}
-                    type="number"
-                    placeholder={app.translator.trans('reflar-doorman.admin.page.doorkey.max_uses')}
-                    onchange={m.withAttr('value', this.updateMaxUses.bind(this, this.doorkey))}
-                />
-                {Switch.component({
-                    state: this.doorkey.activates() || false,
-                    onchange: this.updateActivates.bind(this, this.doorkey),
-                    className: 'Doorkey-switch',
-                })}
+            <div className="DoormanSettingsListItem">
+                <div className="DoormanSettingsListItem-inputs">
+                    <input
+                        className="FormControl Doorkey-key"
+                        type="text"
+                        disabled
+                        value={this.doorkey.key()}
+                        placeholder={app.translator.trans('reflar-doorman.admin.page.doorkey.key')}
+                        onchange={m.withAttr('value', this.updateKey.bind(this, this.doorkey))}
+                    />
+                    {Select.component({
+                        options: this.getGroupsForInput(),
+                        className: 'Doorkey-select',
+                        onchange: this.updateGroupId.bind(this, this.doorkey),
+                        value: this.doorkey.groupId() || 3,
+                    })}
+                    <input
+                        className="FormControl Doorkey-maxUses"
+                        value={this.doorkey.maxUses() || '0'}
+                        type="number"
+                        placeholder={app.translator.trans('reflar-doorman.admin.page.doorkey.max_uses')}
+                        onchange={m.withAttr('value', this.updateMaxUses.bind(this, this.doorkey))}
+                    />
+                    {Switch.component({
+                        state: this.doorkey.activates() || false,
+                        onchange: this.updateActivates.bind(this, this.doorkey),
+                        className: 'Doorkey-switch',
+                    })}
+                </div>
+                
                 {Button.component({
                     type: 'button',
                     className: 'Button Button--warning Doorkey-button',
                     icon: 'fa fa-times',
                     onclick: this.deleteDoorkey.bind(this, this.doorkey),
                 })}
+
                 {this.doorkey.maxUses() === this.doorkey.uses()
                     ? Badge.component({
                         className: 'Doorkey-badge',
@@ -52,9 +57,9 @@ export default class DoormanSettingsListItem extends Component {
                     })
                     : this.doorkey.uses() !== 0 ? (
                         <div>
-                            <h3>{app.translator.transChoice('reflar-doorman.admin.page.doorkey.used_times',
+                            <h3 className="Doorkey-left">{app.translator.transChoice('reflar-doorman.admin.page.doorkey.used_times',
                                 remaining,
-                                {remaining})}
+                                {remaining}).join('')}
                             </h3>
                         </div>
                     ) : ''}
