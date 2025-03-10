4d3346c30c88b63d1310b5c19d57a7d3cc6d6ffc
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-03-10 08:45:11 +0100

    chore: remove unused code

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,10 +1,8 @@
 import app from 'flarum/admin/app';
-import SettingsPage from './components/SettingsPage';
 import DoorkeyListPage from './components/DoorkeyListPage';
 
 export { default as extend } from './extend';
 
 app.initializers.add('fof-doorman', () => {
-  app.extensionData.for('fof-doorman').registerPage(SettingsPage);
-  //app.extensionData.for('fof-doorman').registerPage(DoorkeyListPage);
+  app.extensionData.for('fof-doorman').registerPage(DoorkeyListPage);
 });
