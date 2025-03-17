c97c50229fe55f0d2c02d43c3b7c0d45cd799430
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-03-17 09:10:22 +0100

    chore

--- tests/integration/api/ListDoorkeysTestWithSearchTest.php
+++ tests/integration/api/ListDoorkeysTestWithSearchTest.php
@@ -54,7 +54,7 @@ class ListDoorkeysTestWithSearchTest extends TestCase
         parent::tearDown();
 
         $this->database()->table('doorkeys')->delete();
-        $this->database()->table('users')->delete();
+        $this->database()->table('users')->whereIn('id', [2])->delete();
     }
 
     /**
